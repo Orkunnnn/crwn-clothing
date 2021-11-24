14fe15c8bd90617e31e817e8b96ebe4421ba0cee
orkunnnn <orkunkonak1414@gmail.com>
2021-11-24 17:39:08 +0300

    created project with create-react-app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from "./components/homepage.component";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
