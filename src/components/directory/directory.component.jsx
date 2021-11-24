54093d531aa175d2ac13e892f736ba72d70734ef
orkunnnn <orkunkonak1414@gmail.com>
2021-11-24 19:18:29 +0300

    broken our homepage into smaller components, finished styling our menu-item component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import MenuItem from "../menu-item/menu-item.component";
+
+import "./directory.styles.scss";
+
+class Directory extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sections: [
+        {
+          id: 1,
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+        },
+        {
+          id: 2,
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+        },
+        {
+          id: 3,
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+        },
+        {
+          id: 4,
+          title: "womens",
+          size: "large",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+        },
+        {
+          id: 5,
+          title: "mens",
+          size: "large",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, title, size, imageUrl }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
